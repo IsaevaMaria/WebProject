c01a7e799f115f59db6eba55e087c82421207439
IsaevaMaria <isaevam2015@gmail.com>
2020-04-25 18:07:38 +0300

    доделана главная страница

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+
+
+
 from flask import Flask, render_template
+from data import db_session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = "Ученик Яндекс.Лицея"
+    user = 'Maria'
+
     return render_template('index.html', title='Домашняя страница',
                            username=user)
 
@@ -15,4 +20,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    #db_session.global_init("db/blogs.sqlite")
+    app.run(port=8000, host='127.0.0.1')
