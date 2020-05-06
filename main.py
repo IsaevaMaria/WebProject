c207b23004444944da098c35f871b6aa7df48a1b
IsaevaMaria <isaevam2015@gmail.com>
2020-05-06 19:11:13 +0300

    Добавлена страница Об объекте

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from data import db_session
 from data.building import Building
 from data.categories import Categories
 from data.routes import Routes
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-bootstrap = Bootstrap(app)
 
 def main():
     db_session.global_init("db/my_city.sqlite")
 
 
-    app.run(port=8000, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1')
     #session = db_session.create_session()
     #user = session.query(Categories).first()
     #for user in session.query(Building).filter(Building.categories_id == 1):
@@ -27,13 +25,14 @@ def main():
 def index():
     return render_template('index.html', title='Мой город - Кострома')
 
+
 @app.route('/attractions')
 def attractions():
     session = db_session.create_session()
     b = session.query(Building).filter(Building.categories_id == 1)
     return render_template('attractions.html', title='Достопримечательности Костромы', items=b)
 
-@app.route('/routes')
+@app.route('/kostroma_routes')
 def routes():
     session = db_session.create_session()
     b = session.query(Routes).all()
@@ -45,6 +44,12 @@ def slider(rout_id):
     b = session.query(Building).filter(Building.routes_id == rout_id)
     return render_template('carusel.html', title='Прогулки по Костроме', items=b)
 
+@app.route('/attraction_id/<int:attrac_id>')
+def attraction_id(attrac_id):
+    session = db_session.create_session()
+    b = session.query(Building).filter(Building.id == attrac_id).first()
+    print(b.id)
+    return render_template('attracabout.html', title='Прогулки по Костроме', item=b)
 
 if __name__ == '__main__':
     main()
