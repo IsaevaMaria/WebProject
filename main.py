cea012588c3eff8c3d1a2bad92b7b73117cc08cf
IsaevaMaria <isaevam2015@gmail.com>
2020-05-06 15:30:12 +0300

    слайдер маршрутов

--- main.py
+++ main.py
@@ -3,30 +3,14 @@ from data import db_session
 from data.building import Building
 from data.categories import Categories
 from data.routes import Routes
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+bootstrap = Bootstrap(app)
 
 def main():
     db_session.global_init("db/my_city.sqlite")
-    a1 = Routes()
-    a1.title = 'Ансамбль Сусанинской площади'
-    a2 = Routes()
-    a2.title = 'Прогулка по центру'
-    a3 = Routes()
-    a3.title = 'Прогулка по ул.Советской'
-    a4 = Routes()
-    a4.title = 'Прогулка по ул. Симановского'
-    a5 = Routes()
-    a5.title = 'Прогулка по проспекту Мира'
-    a6 = Routes()
-    a6.title = 'Торговые ряды'
-    a7 = Routes()
-    a7.title = 'Места Н.А. Островского'
-    a8 = Routes()
-    a8.title = 'Приезд Екатерины II'
-    a9 = Routes()
-    a9.title = 'Династия Романовых'
 
 
     app.run(port=8000, host='127.0.0.1')
@@ -47,7 +31,6 @@ def index():
 def attractions():
     session = db_session.create_session()
     b = session.query(Building).filter(Building.categories_id == 1)
-    print(b)
     return render_template('attractions.html', title='Достопримечательности Костромы', items=b)
 
 @app.route('/routes')
@@ -56,6 +39,11 @@ def routes():
     b = session.query(Routes).all()
     return render_template('route.html', title='Прогулки по Костроме', items=b)
 
+@app.route('/slider/<int:rout_id>')
+def slider(rout_id):
+    session = db_session.create_session()
+    b = session.query(Building).filter(Building.routes_id == rout_id)
+    return render_template('carusel.html', title='Прогулки по Костроме', items=b)
 
 
 if __name__ == '__main__':
